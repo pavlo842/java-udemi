057eb1f5b6374b004e80339da71ae7b1e9572fb6
Pavel <pishchalaups@belhard.com>
2020-01-05 20:24:01 +0300

    Lesson2 + homework

---
+++ src/lesson2/HomeWork.java
@@ -0,0 +1,76 @@
+package lesson2;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        // ЦЕЛЫЕ числа
+
+        // byte
+        byte b1Dec = 12;
+        byte b2Oct = 014;
+        byte b3Bin = 0B1100;
+        byte b4Hex = 0XC;
+
+//        System.out.println(b1Dec);
+//        System.out.println(b2Oct);
+//        System.out.println(b3Bin);
+//        System.out.println(b4Hex);
+
+        // short
+        short sh1Dec = -1300;
+//        short sh2Oct = 02424;
+        short sh3Bin = -0B10100010100;
+        short sh4Hex = -0x000514;
+
+//        System.out.println(sh4Hex);
+
+        // int
+        int i1 = 0;
+        int i2Oct = 0;
+        int i3Bin = 0B0;
+        int i4Hex = 0X0;
+
+//        System.out.println(i1);
+//        System.out.println(i2Oct);
+//        System.out.println(i3Bin);
+//        System.out.println(i4Hex);
+
+        // long
+        long l1 = 123456789L;
+//        long l2Oct = 0‭726746425‬;
+        long l3Bin = 0B111010110111100110100010101;
+        long l4Hex = 0X75BCD15;
+
+//        System.out.println(l1);
+////        System.out.println(l2Oct);
+//        System.out.println(l3Bin);
+//        System.out.println(l4Hex);
+
+        // float
+        float f1 = 2.5f;
+        float f2 = 30;
+
+//        System.out.println(f2);
+
+        // double
+        double d1 = 2.5;
+        double d2 = 40;
+
+//        System.out.println(d2);
+
+        // boolean
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+//        System.out.println(bool1);
+//        System.out.println(bool2);
+
+        // char
+        char ch1 = '\u1256';
+        char ch2 = 'h';
+        char ch3 = 6894;
+
+//        System.out.println(ch3);
+    }
+
+}
