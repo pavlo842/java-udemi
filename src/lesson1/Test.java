4dd0144d451f83e9f2667c586c81fc749485d0a3
Pavlo842 <pavlo842@mail.ru>
2020-01-05 16:20:14 +0300

    lesson1

---
+++ src/lesson1/Test.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("                   Рубаи");
+        System.out.println("Много лет размышлял я над жизнью земной...");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                Омар Хайям");
+
+    }
+
+}
