b02468c0f18c6e51bdb7d5f8fb998c265f076af2
Pall <pavlo842@mail.ru>
2020-01-07 11:09:07 +0300

    homework lesson4

---
+++ src/lesson4/homework4/Student.java
@@ -0,0 +1,13 @@
+package lesson4.homework4;
+
+public class Student {
+
+    int numberOfStudentsTicket;
+    String firstName;
+    String lastName;
+    int yearOfStudy = 2019;
+    double averageMathScore;
+    double averageEconomicScore;
+    double averageRatingInForeignLanguage;
+
+}
