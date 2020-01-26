1da7741592b5ccb1a86684d893f3451fa15c1883
Pavel <pishchalaups@belhard.com>
2020-01-26 20:36:14 +0300

    lesson9

--- src/lesson9/Car.java
+++ src/lesson9/Car.java
@@ -1,24 +1,40 @@
 package lesson9;
 
 public class Car {
+
+    int z = 5;
+
     String color;
     String engine;
     static int count;
 
     Car(String color2, String engine2) {
-        count++;
-        color = color2;
-        engine = engine2;
+        this.count++;
+        this.color = color2;
+        this.engine = engine2;
     }
 
-    public void showColor() {
-        System.out.println("Color car " + "color");
+//    public void showColor() {
+//        System.out.println("Color car " + color);
+//    }
+//
+//    public void changeColor(String color3) {
+//        System.out.println("Color car is empty");
+//        int cena = 5000;
+//        color = color3;
+//        cena += 1000;
+//    }
+
+    void changeColor(String color) {
+        this.color = color;
+        System.out.println(color);
     }
 
-    public void changeColor(String color3) {
-        System.out.println("Color car is empty");
-        int cena = 5000;
-        color = color3;
-        cena += 1000;
+    public static void main(String[] args) {
+        Car c = new Car("red", "V6");
+        System.out.println(c.color);
+        c.changeColor("black");
+        System.out.println(c.color);
     }
+
 }
---
+++ src/lesson9/Student.java
@@ -0,0 +1,20 @@
+package lesson9;
+
+public class Student {
+}
+
+class StudentTest {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3;
+
+        // прекращение жизни объекта
+        // первый вариант
+        st1 = null;
+        // второй вариант
+        // st1 = new Student1(); // перезаписать переменную на новый объект
+        
+
+    }
+}
