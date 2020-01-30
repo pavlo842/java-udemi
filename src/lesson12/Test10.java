a09ee3177cc9d82a68252e021f3d6b3cb04fed97
pavel <pishchalaups@belhard.com>
2020-01-30 08:55:26 +0300

    todo

---
+++ src/lesson12/Test10.java
@@ -0,0 +1,29 @@
+package lesson12;
+
+public class Test10 {
+    public static void main(String[] args) {
+//        int a = 30;
+//        if (a < 20) { // Если использовать одно выражение, то фигурные скобки не обязательны
+//            System.out.println("a menshe 20");
+//        } else {
+//            System.out.println("a NE menshe 20");
+//        }
+
+//        int a = 10;
+//        if (a < 20)  // Сдесь всегда true
+//            System.out.println("a menshe 20");
+//        else // Сдесь всегда false
+//            System.out.println("a NE menshe 20");
+//        System.out.println("Hello");
+
+        int salary = 400;
+        if (salary >= 500) {
+            System.out.println(":)");
+        } else { // Сдесь всегда false
+            System.out.println(":(");
+            System.out.println("Hello");
+        }
+
+        продолжить на 24-50
+    }
+}
