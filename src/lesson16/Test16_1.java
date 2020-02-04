f7318d3715ab512ca2146d51ed3b1cd24677fc74
pavel <pishchalaups@belhard.com>
2020-02-04 09:02:24 +0300

    lesson16 string TODO

---
+++ src/lesson16/Test16_1.java
@@ -0,0 +1,16 @@
+package lesson16;
+
+public class Test16_1 {
+    public static void main(String[] args) {
+        // 1-й вариант создания объекта класса String
+        String s1 = new String("Good day!");
+        System.out.println(s1);
+
+        // 2-й вариант создания объекта класса String
+        String s2 = "Hello";
+        System.out.println(s2);
+
+    }
+}
+
+31.00
