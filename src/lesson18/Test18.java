270009b2711c34037d59e6b295d95155a7c0c564
pavel <pishchalaups@belhard.com>
2020-02-14 10:11:36 +0300

    lesson18 array TODO 1:00

--- src/lesson18/Test18.java
+++ src/lesson18/Test18.java
@@ -8,11 +8,13 @@ public class Test18 {
         String [] array2; // Обявление массива с типом String
         double [][] array3;  // Обявление двумерного массива с типом double
         int [][] array10;
+        double [] array11;
         // ДЛИНА МАССИВА ВСЕГДА int
         array1 = new int[8]; // Создание массива с длинной 8
         array2 = new String[3]; // Создание массива с длинной 3
         array3 = new double[4][2]; // Создание четырех одномерных массива с длинной каждого 2
         array10 = new int[3][]; // Создание три одномерных массива с неопределленой длиной
+        array11 = new double[2];
         System.out.println(array2.length);
 
         // ИНИЦИАЛИЗАЦИЯ
@@ -27,9 +29,9 @@ public class Test18 {
         array5[0] = 2.5;
         array5[1] = 3.5;
 
+        array11 = array5;
+
         array3[1] = array5; // Присвоение одномерного массива двумерному
         System.out.println(array3[1][0]);
     }
 }
-
-44.00
